f0c5908a202d96730b94468acc5bd61a9aba8a30
Branko Visser <b.visser@kwigy.com>
2021-08-16 15:33:50 +0200

    emptySpringProject

---
+++ src/main/java/com/example/cryptobank/CryptobankApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptobankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptobankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cryptobank/CryptobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptobankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
