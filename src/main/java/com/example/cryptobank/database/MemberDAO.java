bd22022dbc5728ac146a0a788dffaf526ec49a8e
Branko Visser <b.visser@kwigy.com>
2021-08-18 10:17:19 +0200

    nu inc de comment

--- src/main/java/com/example/cryptobank/database/MemberDAO.java
+++ src/main/java/com/example/cryptobank/database/MemberDAO.java
@@ -1,2 +1,6 @@
-package com.example.cryptobank.database;public class MemberDAO {
+package com.example.cryptobank.database;
+
+public class MemberDAO {
+
+    //Checken of de package nu wel naar Git meekomt
 }
