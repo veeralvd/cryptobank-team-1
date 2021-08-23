322d8dd42cd309af988fe9f3927e3f840058b8ae
Mark Broekman <mark.broekman@hva.nl>
2021-08-23 20:04:46 +0200

    Kleine aanpassing CustomerService

--- src/main/java/com/example/cryptobank/service/CustomerService.java
+++ src/main/java/com/example/cryptobank/service/CustomerService.java
@@ -1,18 +1,31 @@
 package com.example.cryptobank.service;
 
+import com.example.cryptobank.database.AdminDAO;
+import com.example.cryptobank.database.CustomerDAO;
+import com.example.cryptobank.domain.Admin;
+import com.example.cryptobank.domain.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
 
-    //private DAO -> invoegen na push
+    private AdminDAO customerDAO;
 
     private final Logger logger = LoggerFactory.getLogger(CustomerService.class);
 
-    public CustomerService() {
+    @Autowired
+    public CustomerService(AdminDAO customerDAO) {
+        this.customerDAO = customerDAO;
         logger.info("New CustomerService");
+    }
+
+    public boolean checkIfUserCanBeRegistred(String username) {
+        // TODO: 23/08/2021 checken, want vereist een Admin ->  Customer adminToCheck = customerDAO.findByUsername(username);
+        //return adminToCheck == null;
+        return false;
     }
 
 }
