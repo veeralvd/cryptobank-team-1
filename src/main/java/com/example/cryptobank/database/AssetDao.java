500e09e024e96dc20686380a1cba6c222fb994b7
Sarah-Jayne Nogarede <s.j.nogarede@hotmail.com>
2021-08-24 17:04:58 +0200

    Interface AssetDao gemaakt

--- src/main/java/com/example/cryptobank/database/AssetDao.java
+++ src/main/java/com/example/cryptobank/database/AssetDao.java
@@ -1,2 +1,16 @@
-package com.example.cryptobank.database;public interface AssetDao {
+package com.example.cryptobank.database;
+
+import com.example.cryptobank.domain.Asset;
+
+/**
+ * @author Sarah-Jayne Nogarede
+ * Dit is de DAO interface voor het object 'Asset', waarin wordt aangegeven welke methoden een
+ * AssetDao moet implementeren.
+ */
+public interface AssetDao {
+
+    Asset save(Asset asset);
+    Asset findByAbbreviation(String abbreviation);
+    Asset findByName(String name);
+
 }
