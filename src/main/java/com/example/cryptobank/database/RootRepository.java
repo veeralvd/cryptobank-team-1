d435e8a1c4ec21fb3fb84b6a64f1521ae144be2f
Sarah-Jayne Nogarede <s.j.nogarede@hotmail.com>
2021-08-25 14:22:41 +0200

    AssetController aangemaakt, methoden uit JdbcAssetDao aan RootRepository toegevoegd

--- src/main/java/com/example/cryptobank/database/RootRepository.java
+++ src/main/java/com/example/cryptobank/database/RootRepository.java
@@ -1,16 +1,21 @@
 package com.example.cryptobank.database;
 
 import com.example.cryptobank.domain.Admin;
+import com.example.cryptobank.domain.Asset;
+import com.example.cryptobank.domain.Purchase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 public class RootRepository {
     private final Logger logger = LoggerFactory.getLogger(RootRepository.class);
     private AdminDAO adminDAO;
 //    private CustomerDAO customerDAO;
+    private AssetDao assetDao;
 
     @Autowired
     public RootRepository(AdminDAO adminDAO) {
@@ -27,4 +32,23 @@ public class RootRepository {
         return adminDAO.save(admin);
     }
 
-}
+    public Asset save(Asset asset) {
+        return assetDao.save(asset);
+    }
+
+    public Asset getByAbbreviation(String abbreviation) {
+        Asset asset = assetDao.findByAbbreviation(abbreviation);
+        return asset;
+    }
+
+    public Asset getByName(String name) {
+        Asset asset = assetDao.findByName(name);
+        return asset;
+    }
+
+    public ArrayList<Asset> getAll() {
+        ArrayList allAssets = assetDao.getAll();
+        return allAssets;
+    }
+
+} // end of class RootRepository
