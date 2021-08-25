ebe8508b81e11bbf39416ac3e34b7e9d21b24f6c
Branko Visser <b.visser@kwigy.com>
2021-08-25 11:44:01 +0200

    comments bij klasse geplaatst

--- src/main/java/com/example/cryptobank/domain/CryptoCurrencyRate.java
+++ src/main/java/com/example/cryptobank/domain/CryptoCurrencyRate.java
@@ -1,8 +1,13 @@
 package com.example.cryptobank.domain;
 
+/*@author Branko
+* Deze klasse is gemaakt om de koers van een asset te beschrijven.
+* De koers heeft twee parameters, een waarde en een datum met tijdstip van die waarde.
+* Dit heb ik gedaan omdat we het op die manier in de DB gaan vastleggen. Wanneer een asset wordt aangemaakt
+* moet daar een koerswaarde en een tijdstip van die waarde bij worden toegekend/vastgelegd.*/
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.time.LocalDateTime;
 
 public class CryptoCurrencyRate {
