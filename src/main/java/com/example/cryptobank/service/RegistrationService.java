6a169e0b3fc2462b0a561f13346427f170eae840
Mark Broekman <mark.broekman@hva.nl>
2021-08-27 10:09:52 +0200

    registrationservice

---
+++ src/main/java/com/example/cryptobank/service/RegistrationService.java
@@ -0,0 +1,23 @@
+package com.example.cryptobank.service;
+
+import com.example.cryptobank.domain.Admin;
+import com.example.cryptobank.domain.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RegistrationService {
+    private final Logger logger = LoggerFactory.getLogger(RegistrationService.class);
+
+    public RegistrationService() {
+        logger.info("New RegistrationService");
+    }
+
+    public Admin register(Admin admin) {
+
+    }
+
+    public Customer register(Customer customer) {
+
+    }
+
+}
