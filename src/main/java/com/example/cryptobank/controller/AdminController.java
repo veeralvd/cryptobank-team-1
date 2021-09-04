c8a59126fb0c69b9246e5d90f364ebd2b29478ba
Branko Visser <b.visser@kwigy.com>
2021-09-04 11:06:11 +0200

    committing minor changes before merging to develop

--- src/main/java/com/example/cryptobank/controller/AdminController.java
+++ src/main/java/com/example/cryptobank/controller/AdminController.java
@@ -5,7 +5,6 @@ import com.example.cryptobank.service.AdminService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
