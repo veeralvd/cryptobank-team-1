a81757b1e0cf4f249744c94322705bfc404751fd
Satish Jethoe <satish.jethoe@hva.nl>
2021-09-07 13:49:10 +0200

    PortfolioDao: updatePositive en updateNegative aangemaakt tbv TransactionService

--- src/main/java/com/example/cryptobank/database/PortfolioDao.java
+++ src/main/java/com/example/cryptobank/database/PortfolioDao.java
@@ -1,8 +1,15 @@
 package com.example.cryptobank.database;
 
+import com.example.cryptobank.domain.Customer;
+import com.example.cryptobank.domain.Transaction;
+
 import java.util.Map;
 
 public interface PortfolioDao {
 
-    public Map<String, Double> getAssetmapByIban (String iban);
+    Map<String, Double> getAssetmapByIban (String iban);
+
+    double updateAssetAmountPositive(double transactionAssetAmount, Customer customer, Transaction transaction);
+
+    double updateAssetAmountNegative(double transactionAssetAmount, Customer customer, Transaction transaction);
 }
