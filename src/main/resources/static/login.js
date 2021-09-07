5a848db47bdcf970936f361189df5654fd13d41f
Branko Visser <b.visser@kwigy.com>
2021-09-07 13:11:34 +0200

    login form hoofdpijn

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -1,11 +1,51 @@
-//@author Branko V
 
 
-function login(event) {
-            let username = document.getElementById('username').value
-            let password = document.getElementById('password').value
-            console.log(username, password)
+
+
+
+async function postFormDataAsJson({url, formData}) {
+
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(plainFormData);
+
+    const fetchOptions = {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: formDataJsonString,
+        mode: 'no-cors',
+    };
+
+    const response = await fetch(url, fetchOptions);
+
+    if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+
+    return response.json();
 }
+async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const url = loginform.action;
+
+    try {
+        const formData = new FormData(form);
+        
+        const responseData = await postFormDataAsJson({url, formData});
+        
+        console.log({responseData})
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const loginform = document.getElementById('loginform')
+
+loginform.addEventListener('submit', handleFormSubmit);
 
 
 
