71ef82fa3e1417cb47a5704edfda495ae7e0bc3e
Branko Visser <b.visser@kwigy.com>
2021-09-11 21:16:30 +0200

    nu wel pages toegevoegd aan commit

---
+++ src/main/resources/templates/resetPassword.js
@@ -0,0 +1,41 @@
+
+document.querySelector('#resetPassword').addEventListener(`click`,
+    function (event){
+        event.preventDefault() // anders wordt de gebruiker naar andere pagina geleidt
+        // // selecteer inputs en zet waarden omde normale submit functie uitgevoerd
+
+
+        let password = String(document.querySelector('#username').value)
+        let passwordConf = String(document.querySelector('#password').value)
+
+        let passwordsAreEqual = Boolean(password === passwordConf);
+
+        // Stuurt username en password naar de server
+        let data = '?password=' + password
+        const url = `http://localhost:8080/login` + data;
+        const options = {
+            method: `POST`,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        // Token komt terug als username & password erkent wordt
+        fetch(url,options)
+            .then(response => {
+                if (response.ok){
+                    response.text();
+                    let accessToken = response.headers.get('Authorization');
+                    let refreshToken = response.headers.get('refresh_token');
+                    console.log(accessToken + "hallo access");
+                    console.log(refreshToken + "hallo refresh");
+                    localStorage.setItem('Authorization', accessToken);
+                    localStorage.setItem('refresh_token', refreshToken);
+                    window.location.assign("http://localhost:8080/assets.html")
+                }else if (!response.ok) {
+                    console.log('username and/or password are incorrect')
+                }})
+            .catch((error) => {
+                console.error('Error ' + error);
+            })
+
+    })
