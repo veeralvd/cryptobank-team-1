a808572c23fe7f0829f1f51ae31c48810c6902a5
Branko Visser <b.visser@kwigy.com>
2021-09-13 17:32:44 +0200

    reset password aangepast

--- src/main/resources/static/resetPassword.js
+++ src/main/resources/static/resetPassword.js
@@ -6,9 +6,12 @@ function getToken(){
     const stringUrl = URL.split(`=`)
     const token = stringUrl[1];
     console.log(token);
-
-    localStorage.setItem('Authorization', token);
-    console.log()
+    if (token === undefined) {
+        document.getElementById('submitNewPasswordForm').style.display="none"
+    } else {
+        localStorage.setItem('Authorization', token);
+        document.getElementById('sumbitEmailForm').style.display="none"
+    }
 }
 
 document.querySelector('#resetPassword').addEventListener(`click`,
@@ -21,38 +24,67 @@ document.querySelector('#resetPassword').addEventListener(`click`,
         let passwordConf = String(document.querySelector('#passwordConf').value)
 
         let passwordsAreEqual = Boolean(password === passwordConf);
-        // Moet de check op gelijke wachtwoorden hier?
+
         if (!passwordsAreEqual) {
+
+            //TODO We moeten nog functionaliteit voor wanneer de wachtwoorden NIET kloppen volgens onze richtlijnen OF
+            // als de wachtwoorden niet gelijk zijn aan elkaar
             console.log('Passwords not the same')
         }
 
-        // Stuurt username en password naar de server
-        let data = '?password=' + password
-        const url = `http://localhost:8080/submitnewpassword` + data;
+        else {
+
+            // Stuurt username en password naar de server
+            let data = '?password=' + password
+            const url = `http://localhost:8080/api/forgot/submit` + data;
+            const options = {
+                method: `POST`,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            // Token komt terug als username & password erkent wordt
+            fetch(url, options)
+                .then(response => {
+                    if (response.ok) {
+                        response.text();
+
+                        // Door naar volgende pagina/scherm
+                        window.location.assign("http://localhost:8080/index.html")
+                    } else if (!response.ok) {
+                        console.log('Failed to save new password')
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error ' + error);
+                })
+        }
+    })
+
+document.getElementById('submitEmail').addEventListener('click',
+    function (event) {
+    event.preventDefault();
+
+    let email = String(document.querySelector('#emailToSubmit').value);
+    let data = '?email=' + email;
+    const url = 'http://localhost:8080/api/forgot/password' + data;
         const options = {
             method: `POST`,
             headers: {
                 'Content-Type': 'application/json'
             }
         }
-        // Token komt terug als username & password erkent wordt
-        fetch(url,options)
+        fetch(url, options)
             .then(response => {
-                if (response.ok){
-                    response.text();
-                    let accessToken = response.headers.get('Authorization');
-                    let refreshToken = response.headers.get('refresh_token');
-                    console.log(accessToken + "hallo access");
-                    console.log(refreshToken + "hallo refresh");
-                    localStorage.setItem('Authorization', accessToken);
-                    localStorage.setItem('refresh_token', refreshToken);
-                    // Door naar volgende pagina/scherm
-                    // window.location.assign("http://localhost:8080/assets.html")
-                }else if (!response.ok) {
-                    console.log('Failed to save new password')
-                }})
+                if (response.ok) {
+                    alert("Request for changing password send.")
+                    window.location.assign("http://localhost:8080/index.html")
+                } else if (response.status.valueOf()=== 418){
+                    document.getElementById('inlogheader').innerHTML = "Email not found";
+                }
+            })
             .catch((error) => {
                 console.error('Error ' + error);
             })
-
     })
+
