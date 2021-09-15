6cde914b476aab44f1a93acf026783bb2faef629
Mark Broekman <mark.broekman@hva.nl>
2021-09-15 10:01:04 +0200

    tussentijds ivm ontbreken registrationDto

--- src/main/resources/static/register.js
+++ src/main/resources/static/register.js
@@ -13,20 +13,6 @@ document.querySelector('#register').addEventListener('click',
         console.log(personalFormDataJsonString)
 
 
-        // let username = String(document.querySelector('#username').value)
-        // let password = String(document.querySelector('#password').value)
-        // let firstName = String(document.querySelector('#firstName').value)
-        // let lastName = String(document.querySelector('#lastName').value)
-        // let dateOfBirth = String(document.querySelector('#dateOfBirth').value)
-        // let socialSecurityNumber = String(document.querySelector('#socialSecurityNumber').value)
-        // let street = String(document.querySelector('#street').value)
-        // let zipcode = String(document.querySelector('#zipcode').value)
-        // let housenumber = String(document.querySelector('#housenumber').value)
-        // let addition = String(document.querySelector('#addition').value)
-        // let city = String(document.querySelector('#city').value)
-        // let email = String(document.querySelector('#email').value)
-
-
         // Stuurt username en password naar de server
         const url = `http://localhost:8080/register`
         const options = {
@@ -52,3 +38,52 @@ document.querySelector('#register').addEventListener('click',
             })
 
     })
+
+const usernameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#password');
+const firstNameInput = document.querySelector('#firstName');
+const lastNameInput = document.querySelector('#lastName');
+const socialSecurityNumberInput = document.querySelector('#socialSecurityNumber');
+const streetInput = document.querySelector('#street');
+const emailInput = document.querySelector('#email');
+const small1 = document.querySelector('#line1');
+
+const isRequired = value => value === '' ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+const isEmailValid = (email) => {
+    const charsNotAllowed = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return charsNotAllowed.test(email);
+}
+const isPasswordOK = (password) => {
+    const toCheck = new RegExp("^(?=.*[a-z])(?=.{8,})");
+    return toCheck.test(password);
+}
+
+const showError = (input, message) => {
+    input.classList.remove('success');
+    input.classList.add('error');
+    const error = input.querySelector('small');
+    error.textContent = message;
+}
+
+const showSuccess = (input) => {
+    input.classList.remove('error');
+    input.classList.add('success');
+    const error = input.querySelector('small');
+    error.textContent = '';
+}
+
+const checkUsername = () => {
+    let valid = false;
+    const min = 4, max = 25;
+
+    if(!isRequired(usernameInput)) {
+        showError(small1, 'Username can not be blank')
+    } else if (!isBetween(usernameInput.length, min, max)) {
+        showError(small1, `Length must be between ${min} and ${max}`)
+    } else {
+        showSuccess(small1);
+        valid = true;
+    }
+    return valid;
+}
