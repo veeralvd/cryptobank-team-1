29f6fbc336644dc3655ec2f077c567210a70d8d2
Branko Visser <b.visser@kwigy.com>
2021-09-16 16:27:19 +0200

    refresh token async and await werkt bij portfolio

--- src/main/resources/static/dashboard.js
+++ src/main/resources/static/dashboard.js
@@ -1,4 +1,4 @@
-const options = {
+let options = {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json',
@@ -6,31 +6,70 @@ const options = {
     }
 }
 
-function getPortfolio() {
+
+ function getPortfolio() {
     const urlPortfolio = "http://localhost:8080/portfolio"
 
-    fetchTable(urlPortfolio, options, "#portfolioTable");
+     fetchTable(urlPortfolio, options, "#portfolioTable");
     document.title = "My Portfolio";
 }
 
-window.onload = getPortfolio();
-
-// document.getElementById("myPortfolio").addEventListener('click',
-//     function (event) {
-//         event.preventDefault()
-//         const urlPortfolio = "http://localhost:8080/portfolio"
-//
-//         //TODO nu leeg scherm bij een 403 geen authorization, moet opnieuw inloggen
-//         fetchTable(urlPortfolio, options, "#portfolioTable");
-//         document.title = "My Portfolio";
-//
-//     }, false);
+document.addEventListener("DOMContentLoaded", function (){
+    getPortfolio();
+})
 
 
+document.querySelector("#myportfolio").addEventListener('click',
+     function (event) {
+        event.preventDefault()
+        const urlPortfolio = "http://localhost:8080/portfolio"
 
-function fetchTable(url, options, tableId) {
-    fetch(url, options)
+        getPortfolio();
+
+     }, false);
+
+async function fetchWithRetry(x, ...args) {
+
+        let firstTry = await x(...args);
+        console.log(firstTry);
+        console.log("In de fetch with retry, na de fetch!!!" )
+        if (firstTry.status === 403) {
+            console.log("toRefresh in ieder geval gestart")
+            const urlRefresh = "http://localhost:8080/refresh"
+            const options = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('refresh_token')
+                }
+            }
+          await fetch(urlRefresh, options)
+                .then(response => {
+                    console.log("fetch aangeroepen");
+                    if (response.ok) {
+                        localStorage.setItem('Authorization', response.headers.get('Authorization'));
+                        localStorage.setItem('refresh_token', response.headers.get('refresh_token'));
+                        updateHeaderOptions();
+                        return  x(...args);
+                    } else {
+                        alert("Session expired. Login to regain access.")
+                        window.location.assign('http://localhost:8080/index.html')
+                    }
+                })
+                .catch((error) => {
+                    console.log('Error: ' + error)
+                })
+
+        }
+    return firstTry;
+
+}
+
+
+ function fetchTable(url, options, tableId) {
+    fetchWithRetry(fetch, url, options)
         .then(response => {
+            console.log("In de fetchTable")
             if (response.ok) {
                 response.json()
                     .then(json => {
@@ -44,7 +83,10 @@ function fetchTable(url, options, tableId) {
                     .catch((error) => {
                         console.error('Error' + error);
                     })
-            } else {
+            } else if(!response.ok) {
+                return response.status
+
+            }else {
                document.querySelector('#portfolio').insertAdjacentText("afterbegin", 'Je portfolio is nog leeg!')
             }
         })
@@ -73,4 +115,8 @@ function generateTable(table, data) {
             cell.appendChild(text);
         }
     }
+}
+
+function updateHeaderOptions() {
+    options.headers.Authorization = localStorage.getItem('Authorization');
 }
