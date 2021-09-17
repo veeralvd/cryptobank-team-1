42658dd599f36d81b68ad36fe5e9e54868c577f6
Branko Visser <b.visser@kwigy.com>
2021-09-17 09:03:02 +0200

    opzet voor module javascript file storage

--- src/main/resources/static/dashboard.js
+++ src/main/resources/static/dashboard.js
@@ -1,3 +1,5 @@
+import {fetchWithRetry} from "./general.js";
+
 let options = {
     method: 'GET',
     headers: {
@@ -28,42 +30,6 @@ document.querySelector("#myportfolio").addEventListener('click',
 
      }, false);
 
-async function fetchWithRetry(x, ...args) {
-
-        let firstTry = await x(...args);
-        console.log(firstTry);
-        console.log("In de fetch with retry, na de fetch!!!" )
-        if (firstTry.status === 403) {
-            console.log("toRefresh in ieder geval gestart")
-            const urlRefresh = "http://localhost:8080/refresh"
-            const options = {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': localStorage.getItem('refresh_token')
-                }
-            }
-          await fetch(urlRefresh, options)
-                .then(response => {
-                    console.log("fetch aangeroepen");
-                    if (response.ok) {
-                        localStorage.setItem('Authorization', response.headers.get('Authorization'));
-                        localStorage.setItem('refresh_token', response.headers.get('refresh_token'));
-                        updateHeaderOptions();
-                        return  x(...args);
-                    } else {
-                        alert("Session expired. Login to regain access.")
-                        window.location.assign('http://localhost:8080/index.html')
-                    }
-                })
-                .catch((error) => {
-                    console.log('Error: ' + error)
-                })
-
-        }
-    return firstTry;
-
-}
 
 
  function fetchTable(url, options, tableId) {
@@ -117,6 +83,6 @@ function generateTable(table, data) {
     }
 }
 
-function updateHeaderOptions() {
-    options.headers.Authorization = localStorage.getItem('Authorization');
-}
+
+
+
