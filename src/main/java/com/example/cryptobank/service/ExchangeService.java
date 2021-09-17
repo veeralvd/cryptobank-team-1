02849d413a2f03e737b9c7c9b219690cc76eb3d8
Mark Broekman <mark.broekman@hva.nl>
2021-09-17 15:13:53 +0200

    comments verwijderd in exchangeservice

--- src/main/java/com/example/cryptobank/service/ExchangeService.java
+++ src/main/java/com/example/cryptobank/service/ExchangeService.java
@@ -19,8 +19,6 @@ public class ExchangeService {
     }
 
     public double getCurrentRateByAbbreviation(String abbreviation) {
-        // query voor alle Rates van alle Assets (Map aanmaken en vullen)
-        // Map. Entryset. Als getKey.equals abbrev. -> extract value (currentRate van Asset in kwestie)
         currentRates = rootRepository.getAllCurrentRates();
         return currentRates.get(abbreviation);
     }
